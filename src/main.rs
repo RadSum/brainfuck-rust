68a12626429bdb983d62a3570f8af5afaf9b98d8
Radek Šumpík <rada.sumpik@gmail.com>
2025-04-05 16:29:29 +0200

    implemented almost every instruction + hello_world example

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::process::exit;
 
 mod tokenizer;
-use tokenizer::to_tokens;
+use tokenizer::{to_tokens, Token};
 
 fn main() {
     let Some(filename) = std::env::args().nth(1) else {
@@ -17,11 +17,38 @@ fn main() {
     let tokens = to_tokens(&file_content);
     
     if let Some(tokens) = tokens {
-        for token in tokens {
-            println!("{:?}", token);
-        }
+        run_program(&tokens);
     } else {
         eprintln!("Error while parsing");
     }
 }
 
+fn run_program(program: &Vec<Token>) -> () {
+    let mut pc = 0;
+    let mut memory: [u8; 256] = [0; 256];
+    let mut memory_pointer: usize = 0;
+
+    while pc < program.len() {
+        match program[pc] {
+            Token::LeftArrow => memory_pointer -= 1,
+            Token::RightArrow => memory_pointer += 1,
+            Token::LeftBracket(_pc) => {
+                if memory[memory_pointer] == 0 {
+                    pc = _pc;
+                    continue;
+                }
+            },
+            Token::RightBracket(_pc) => {
+                if memory[memory_pointer] != 0 {
+                    pc = _pc;
+                    continue;
+                }
+            },
+            Token::Plus => memory[memory_pointer] = memory[memory_pointer].wrapping_add(1),
+            Token::Minus => memory[memory_pointer] = memory[memory_pointer].wrapping_sub(1),
+            Token::Dot => print!("{}", memory[memory_pointer] as char),
+            Token::Comma => todo!(),
+        }
+        pc += 1;
+    }
+}
