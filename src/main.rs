d156dffe3f614f2211194b554797b35a2bc0ca18
Radek Šumpík <rada.sumpik@gmail.com>
2025-04-06 19:21:54 +0200

    implemented all instructions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::process::exit;
+use std::io::{Read, Write};
 
 mod tokenizer;
 use tokenizer::{to_tokens, Token};
+
+const MEMORY_SIZE: usize = 30_000;
 
 fn main() {
     let Some(filename) = std::env::args().nth(1) else {
@@ -25,7 +28,7 @@ fn main() {
 
 fn run_program(program: &Vec<Token>) -> () {
     let mut pc = 0;
-    let mut memory: [u8; 256] = [0; 256];
+    let mut memory: [u8; MEMORY_SIZE] = [0; MEMORY_SIZE];
     let mut memory_pointer: usize = 0;
 
     while pc < program.len() {
@@ -46,9 +49,20 @@ fn run_program(program: &Vec<Token>) -> () {
             },
             Token::Plus => memory[memory_pointer] = memory[memory_pointer].wrapping_add(1),
             Token::Minus => memory[memory_pointer] = memory[memory_pointer].wrapping_sub(1),
-            Token::Dot => print!("{}", memory[memory_pointer] as char),
-            Token::Comma => todo!(),
+            Token::Dot => {
+                print!("{}", memory[memory_pointer] as char);
+                std::io::stdout().flush().expect("error while flushing `stdout`");
+            },
+            Token::Comma => memory[memory_pointer] = read_byte(),
         }
         pc += 1;
     }
+}
+
+fn read_byte() -> u8 {
+    std::io::stdin()
+        .bytes()
+        .next()
+        .and_then(|r| r.ok())
+        .unwrap()
 }
